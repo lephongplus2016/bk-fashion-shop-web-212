6278e5cf3074293c077763818df2fbeceaaedacc
lephongplus2016 <lephongplus2016@gmail.com>
2022-03-24 16:34:33 +0700

    add config

---
+++ lib/session.php
@@ -0,0 +1,55 @@
+<?php
+/**
+*Session Class
+**/
+class Session{
+ public static function init(){
+  if (version_compare(phpversion(), '5.4.0', '<')) {
+        if (session_id() == '') {
+            session_start();
+        }
+    } else {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+ }
+
+ public static function set($key, $val){
+    $_SESSION[$key] = $val;
+ }
+
+ public static function get($key){
+    if (isset($_SESSION[$key])) {
+     return $_SESSION[$key];
+    } else {
+     return false;
+    }
+ }
+
+ public static function checkSession(){
+    self::init();
+    if (self::get("adminlogin")== false) {
+     self::destroy();
+     header("Location:login.php");
+    }
+ }
+
+// khởi tạo đối tượng session lưu login, khi nào có đối tượng này thì là đang login
+// -> sẽ dùng để validation các trang index của login sau này
+ public static function checkLogin(){
+    self::init();
+    if (self::get("login")== true) {
+     header("Location:index.php");
+    }
+ }
+
+// hủy session login đối
+// buộc phải login để vào những site nào có checkSession-> 
+ public static function destroy(){
+  session_destroy();
+  header("Location:login.php");
+ }
+}
+?>
+
