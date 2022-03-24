08f344495a7ee2559bc15e9927a15a3a9da976fe
lephongplus2016 <lephongplus2016@gmail.com>
2022-03-24 20:48:49 +0700

    code register feature

--- classes/user.php
+++ classes/user.php
@@ -18,11 +18,49 @@ class user
 		$this->fm = new Format();
 	}
 
-	public function show_customers(){
+	public function show_user(){
         $query = "SELECT * FROM tbl_user";
         $result = $this->db->select($query);
         return $result; 
         // return "abd";
+    }
+    public function insert_user($data){
+        // return $data;
+        $name = mysqli_real_escape_string($this->db->link, $data['name']);
+        $email = mysqli_real_escape_string($this->db->link, $data['email']);
+        $address = mysqli_real_escape_string($this->db->link, $data['address']);
+        $phone = mysqli_real_escape_string($this->db->link, $data['phone']);
+        // password mã hóa md5
+        $password = mysqli_real_escape_string($this->db->link, md5($data['password']));
+        $password2 = mysqli_real_escape_string($this->db->link, md5($data['password2']));
+        if ($password != $password2){
+            $alert = "<span class='error'>Password nhập lại không đúng!</span>";
+            return $alert;
+        }
+
+        // kiểm tra dữ liệu rỗng
+        if($name==""  || $email=="" || $address=="" || $phone =="" || $password ==""){
+            $alert = "<span class='error'>Các trường không được rỗng</span>";
+            return $alert;
+        }else{
+            // đặc biệt email là trường unique nên cần check riêng
+            $check_email = "SELECT * FROM tbl_user WHERE email='$email' LIMIT 1";
+            $result_check = $this->db->select($check_email);
+            if($result_check){
+                $alert = "<span class='error'>Email Already Existed ! Please Enter Another Email</span>";
+                return $alert;
+            }else{
+                $query = "INSERT INTO tbl_user(name,email,address,phone,password, role) VALUES('$name','$email', '$address', '$phone', '$password','customer')";
+                $result = $this->db->insert($query);
+                if($result){
+                    $alert = "<span class='success'>Tạo tài khoản thành công!</span>";
+                    return $alert;
+                }else{
+                    $alert = "<span class='error'>Tạo tài khoản thất bại, vui lòng thử lại!</span>";
+                    return $alert;
+                }
+            }
+        }
     }
 
 
