87c069c83815a4419f2ee5b63b08c10aa06bb7ad
lephongplus2016 <lephongplus2016@gmail.com>
2022-04-16 11:38:41 +0700

    pagination for shop page

--- shop.php
+++ shop.php
@@ -32,7 +32,7 @@
             <div class="row">
 
 <?php 
-    $getProduct = $product->show_product();
+    $getProduct = $product->show_product_by_pagination();
     if($getProduct){
         while($row = $getProduct->fetch_assoc()){
             $image_list = $product->getImgByProductId($row['productId']);
@@ -42,9 +42,6 @@
                     break;
                 }
 ?>
-
-
-                    
 
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
@@ -57,33 +54,14 @@
                                 <div class="product__item__text">
                                     <h6><?php echo $row['productName'] ; ?></h6>
                                     <a href="#" class="add-cart">+ Thêm vào giỏ hàng</a>
-                                   <!--  <div class="rating">
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                    </div> -->
+                                   
                                     <h5><?php echo $row['price'] ; ?> VNĐ</h5>
-                                    <!-- <div class="product__color__select">
-                                        <label for="pc-4">
-                                            <input type="radio" id="pc-4">
-                                        </label>
-                                        <label class="active black" for="pc-5">
-                                            <input type="radio" id="pc-5">
-                                        </label>
-                                        <label class="grey" for="pc-6">
-                                            <input type="radio" id="pc-6">
-                                        </label>
-                                    </div> -->
+                                    
                                 </div>
                             </div>
                         </div>
 
-                       
-                    
-
-
+  
 <?php             
         }
     }
@@ -91,15 +69,41 @@
 ?>
 
         </div>
-
+                    <!-- pagination -->
                     <div class="row">
-                        <div class="col-lg-12">
+                        <?php
+                                    $number_of_product_per_page = 1;
+                                    if(!isset($_GET['page'])){
+                                        $page = 1;
+                                    }else{
+                                        $page = $_GET['page'];
+                                    }
+                                    $product_all = $product->show_product(); 
+                                    $num_of_product = mysqli_num_rows($product_all);
+                                    $num_of_page = ceil($num_of_product/$number_of_product_per_page);
+                                    $index_page_show = ($page-1)*$number_of_product_per_page +1;
+
+                                ?>
+                        <div class="col-lg-6">
+                            <!-- phần thống kê hiển thị sản phẩm -->
+                            <p>Hiển thị <?php echo $index_page_show."-".($index_page_show+$number_of_product_per_page-1); ?> trong tổng số <?php echo  $num_of_product; ?> kết quả</p>
+                        </div>
+                        <div class="col-lg-6">
                             <div class="product__pagination">
-                                <a class="active" href="#">1</a>
-                                <a href="#">2</a>
-                                <a href="#">3</a>
-                                <span>...</span>
-                                <a href="#">21</a>
+                                <!-- trang trước -->
+                                <a href="shop.php?page=<?php if($page>1) {echo $page-1;}  else {echo $page;}?>" ><</a>
+                                <?php
+                                    // số trang hiển thị ra màn hình tối đa hiện tại là 3
+                                    $start = $page> 1? $page -1: $page;
+                                    $end = $page < $num_of_page? $page +1: $num_of_page;
+                                    for($i=$start;$i<=$end;$i++){
+                                        ?>
+                                        <a <?php if($i == $page) { echo 'class="active"';} ?> href="shop.php?page=<?php echo $i ?>"><?php echo $i ?></a>
+                                    <?php
+                                    }
+                                ?>
+                                <!-- trang sau -->
+                                <a href="shop.php?page=<?php if($page<$num_of_page) {echo $page+1;}  else {echo $num_of_page;}?>" >></a>
                             </div>
                         </div>
                     </div>
