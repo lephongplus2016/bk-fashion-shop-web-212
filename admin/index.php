71a53caff30aadba96afe6293a6b68be5bc6baff
Võ Minh Khoa <vmkkhoa@gmail.com>
2022-06-07 22:40:27 +0700

    update btn style

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Đơn đặt hàng</h5>
                                     <p class="card-text">Quản lý các đơn đặt hàng của khách hàng.</p>
-                                    <a href="orderlist.php" class="btn btn-primary">Danh sách đơn đặt hàng</a>
+                                    <a href="orderlist.php" class="btn btn-outline-primary">Danh sách đơn đặt hàng</a>
                                 </div>
                                 </div>
                             </div>
@@ -23,8 +23,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Sản Phẩm</h5>
                                     <p class="card-text">Thêm sản phẩm mới và xem danh sách sản phẩm.</p>
-                                    <a href="productadd.php" class="btn btn-primary">Thêm sản phẩm</a>
-                                    <a href="productlist.php" class="btn btn-primary">Danh sách Sản phẩm</a>
+                                    <a href="productadd.php" class="btn btn-outline-primary">Thêm sản phẩm</a>
+                                    <a href="productlist.php" class="btn btn-outline-primary">Danh sách Sản phẩm</a>
                                 </div>
                                 </div>
                             </div>
@@ -33,8 +33,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Danh mục</h5>
                                     <p class="card-text">Thêm danh mục mới và xem danh sách danh mục.</p>
-                                    <a href="categoryadd.php" class="btn btn-primary">Thêm Danh mục</a>
-                                    <a href="categorylist.php" class="btn btn-primary">Danh sách Danh mục</a>
+                                    <a href="categoryadd.php" class="btn btn-outline-primary">Thêm Danh mục</a>
+                                    <a href="categorylist.php" class="btn btn-outline-primary">Danh sách Danh mục</a>
                                 </div>
                                 </div>
                             </div>
@@ -43,8 +43,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Thương hiệu</h5>
                                     <p class="card-text">Thêm thương hiệu mới và xem danh sách thương hiệu.</p>
-                                    <a href="brandadd.php" class="btn btn-primary">Thêm Thương hiệu</a>
-                                    <a href="brandlist.php" class="btn btn-primary">Danh sách Thương hiệu</a>
+                                    <a href="brandadd.php" class="btn btn-outline-primary">Thêm Thương hiệu</a>
+                                    <a href="brandlist.php" class="btn btn-outline-primary">Danh sách Thương hiệu</a>
                                 </div>
                                 </div>
                             </div>
@@ -53,7 +53,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Khách hàng</h5>
                                     <p class="card-text">Danh sách tài khoản khách hàng.</p>
-                                    <a href="client.php" class="btn btn-primary">Danh sách Khách hàng</a>                                    
+                                    <a href="client.php" class="btn btn-outline-primary">Danh sách Khách hàng</a>                                    
                                 </div>
                                 </div>
                             </div>
@@ -62,8 +62,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Slider</h5>
                                     <p class="card-text">Thêm slider mới và xem danh sách slider.</p>
-                                    <a href="slider-add.php" class="btn btn-primary">Thêm Slider</a>
-                                    <a href="slider-list.php" class="btn btn-primary">Danh sách Slider</a>
+                                    <a href="slider-add.php" class="btn btn-outline-primary">Thêm Slider</a>
+                                    <a href="slider-list.php" class="btn btn-outline-primary">Danh sách Slider</a>
                                 </div>
                                 </div>
                             </div>
@@ -72,8 +72,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Banner</h5>
                                     <p class="card-text">Thêm banner mới và xem danh sách banner.</p>
-                                    <a href="banner-add.php" class="btn btn-primary">Thêm Banner</a>
-                                    <a href="banner-list.php" class="btn btn-primary">Danh sách Banner</a>
+                                    <a href="banner-add.php" class="btn btn-outline-primary">Thêm Banner</a>
+                                    <a href="banner-list.php" class="btn btn-outline-primary">Danh sách Banner</a>
                                 </div>
                                 </div>
                             </div>
@@ -83,8 +83,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Tin tức</h5>
                                     <p class="card-text">Thêm tin tức mới và xem danh sách tin tức.</p>
-                                    <a href="article-add.php" class="btn btn-primary">Thêm tin tức</a>
-                                    <a href="article-list.php" class="btn btn-primary">Danh sách tin tức</a>
+                                    <a href="article-add.php" class="btn btn-outline-primary">Thêm tin tức</a>
+                                    <a href="article-list.php" class="btn btn-outline-primary">Danh sách tin tức</a>
                                 </div>
                                 </div>
                             </div>
